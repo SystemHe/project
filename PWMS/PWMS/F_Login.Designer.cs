42274d7855c29d083b21f3db4030588b7e015ab7
SystemHe <1552914463@qq.com>
2017-09-26 17:32:29 +0800

    projects

---
+++ PWMS/PWMS/F_Login.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace PWMS
+{
+    partial class F_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.butLogin = new System.Windows.Forms.Button();
+            this.butClose = new System.Windows.Forms.Button();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textPass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "用户名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "密码：";
+            // 
+            // butLogin
+            // 
+            this.butLogin.Location = new System.Drawing.Point(39, 199);
+            this.butLogin.Name = "butLogin";
+            this.butLogin.Size = new System.Drawing.Size(75, 23);
+            this.butLogin.TabIndex = 2;
+            this.butLogin.Text = "登录";
+            this.butLogin.UseVisualStyleBackColor = true;
+            this.butLogin.Click += new System.EventHandler(this.butLogin_Click);
+            // 
+            // butClose
+            // 
+            this.butClose.Location = new System.Drawing.Point(174, 199);
+            this.butClose.Name = "butClose";
+            this.butClose.Size = new System.Drawing.Size(75, 23);
+            this.butClose.TabIndex = 3;
+            this.butClose.Text = "取消";
+            this.butClose.UseVisualStyleBackColor = true;
+            this.butClose.Click += new System.EventHandler(this.butClose_Click);
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(86, 62);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(184, 25);
+            this.textName.TabIndex = 4;
+            this.textName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textName_KeyPress);
+            // 
+            // textPass
+            // 
+            this.textPass.Location = new System.Drawing.Point(86, 124);
+            this.textPass.Name = "textPass";
+            this.textPass.PasswordChar = '*';
+            this.textPass.Size = new System.Drawing.Size(184, 25);
+            this.textPass.TabIndex = 5;
+            this.textPass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textPass_KeyPress);
+            // 
+            // F_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Controls.Add(this.textPass);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.butClose);
+            this.Controls.Add(this.butLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "F_Login";
+            this.Text = "F_Login";
+            this.Activated += new System.EventHandler(this.F_Login_Activated);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button butLogin;
+        private System.Windows.Forms.Button butClose;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.TextBox textPass;
+    }
+}
